d96c585947d6e80fcdfd5e5454a56e0104a50d56
mse-k <37938606+mse-k@users.noreply.github.com>
2022-02-28 22:48:42 -0500

    Update ExampleJavaMod.java

--- src/connwall/ExampleJavaMod.java
+++ src/connwall/ExampleJavaMod.java
@@ -19,10 +19,9 @@ public class ExampleJavaMod extends Mod{
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                dialog.cont.add("behold... Amogus!").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.button("Quite sussy.", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
@@ -30,7 +29,7 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        new ExampleContentList().load();
     }
 
 }
