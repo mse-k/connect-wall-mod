d9ef93099058050d9d65d0d376e9fdd55253d65a
mse-k <37938606+mse-k@users.noreply.github.com>
2022-03-02 03:35:57 -0500

    Update ExBlocks.java

--- src/connwall/content/ExBlocks.java
+++ src/connwall/content/ExBlocks.java
@@ -12,12 +12,20 @@ import static mindustry.type.ItemStack.*;
 public class ExBlocks implements ContentList{
 
     public static Block
-    testWall;
+    testWall, fCopperWall, fTitaniumWall;
 
     public void load(){
         testWall = new ConnWall("shaped-wall"){{
             requirements(Category.defense, with(Items.lead, 6));
             health = 420;
+        }};
+        fCopperWall = new ConnWall("copper-wall"){{
+            requirements(Category.defense, with(Items.copper, 6));
+            health = 80*4;
+        }};
+        fTitaniumWall = new ConnWall("titanium-wall"){{
+            requirements(Category.defense, with(Items.titanium, 6));
+            health = 110*4;
         }};
     }
 }
